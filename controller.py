94d9a225ba518b023c94cfe8d8a67d07e858137c
jarekaha <jarek.loehning@gmx.de>
2022-06-21 22:36:22 +0200

    -Modifications to work with our modified Library -Enabling before running to save power

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-from rpi_python_drv8825.stepper import StepperMotor
+from stepper import StepperMotor
 
 """
 Provides functions to control the robots movements.
@@ -13,33 +13,33 @@ class Controller:
         self.dir_pin1 = 7
         self.dir_pin2 = 25
         self.mode_pin = 20
-        self.step_type = '1/4'
-        self.fullstep_delay = 'fu'
         self.stepper1 = StepperMotor(self.enable_pin,
                                      self.step_pin1,
-                                     self.dir_pin1,
-                                     self.mode_pin,
-                                     self.step_type,
-                                     self.fullstep_delay)
+                                     self.dir_pin1)
         self.stepper2 = StepperMotor(self.enable_pin,
                                      self.step_pin2,
-                                     self.dir_pin2,
-                                     self.mode_pin,
-                                     self.step_type,
-                                     self.fullstep_delay)
+                                     self.dir_pin2)
 
     def step_forward(self, steps: int):
+        self.stepper1.enable(True)
         self.stepper1.run(steps, True)
         self.stepper2.run(steps, False)
+        self.stepper1.enable(False)
 
     def step_backwards(self, steps: int):
+        self.stepper1.enable(True)
         self.stepper1.run(steps, False)
         self.stepper2.run(steps, True)
+        self.stepper1.enable(False)
 
     def rotate_clockwise(self, steps: int):
+        self.stepper1.enable(True)
         self.stepper1.run(steps, False)
         self.stepper2.run(steps, False)
+        self.stepper1.enable(False)
 
     def rotate_counterclockwise(self, steps: int):
+        self.stepper1.enable(True)
         self.stepper1.run(steps, True)
         self.stepper2.run(steps, True)
+        self.stepper1.enable(False)
