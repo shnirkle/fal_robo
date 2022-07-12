7fee59c907653f8b78a5b19dc47907a1d7f5ab6f
jarekaha <jarek.loehning@gmx.de>
2022-07-12 14:09:10 +0200

    try try try

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import RPi.GPIO as GPIO
 from imReco.picture import capImage
 from imReco.classifier import Classifier
 
+def main():
+    try:
+        clf = Classifier()
+        control = Controller()
+        steps = 200
 
-clf = Classifier()
-control = Controller()
-steps = 200
+        while True:
+            frame = capImage()
+            branches = clf.classify(frame)
+            if branches[0] == 1:
+                if branches[1] == 5:
+                    control.rotate_clockwise(steps//2)
+                if branches[1] == 6:
+                    control.rotate_counterclockwise(steps//2)
+            control.step_forward(steps)
+    except:
+        main()
 
-while True:
-    frame = capImage()
-    branches = clf.classify(frame)
-    if branches[0] == 1:
-        if branches[1] == 5:
-            control.rotate_clockwise(steps//2)
-        if branches[1] == 6:
-            control.rotate_counterclockwise(steps//2)
-    control.step_forward(steps)
+main()
