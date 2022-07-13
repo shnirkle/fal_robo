2802240b4bb293978b98eee4a8b620c621cedad9
notna24 <57870327+notna24@users.noreply.github.com>
2022-07-13 10:44:19 +0200

    add web interface

---
+++ web/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+CLS = None
+
+@app.route("/")
+def index():
+    with open("static/index.html") as indexhtml:
+        data = indexhtml.read()
+    return data
+
+@app.route("/image")
+def image():
+    image = CLS.f
+
+
+def run(cls):
+    CLS = cls
+    try:
+        app.run()
+    except Exception as e:
+        return 1
+    return 0
+
+if __name__ == "__main__":
+    app.run()
